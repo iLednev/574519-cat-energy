177db0acdebd0261110f628b7fc03ae4199cfad2
Lednev <iLednevii@gmail.com>
2018-10-16 11:31:47 +0300

    Макет приведён в соответствие со стайлгайдом, доработан скрипт

--- source/js/switcher.js
+++ source/js/switcher.js
@@ -8,40 +8,52 @@ var labels = document.querySelectorAll(".example__label");
 
 var slides = document.querySelectorAll(".example__slide");
 
-label1.addEventListener ("click", function() {
-  if (radio1.checked === false) {
-    line.classList.toggle("example__switch-line--switched");
-  }
-});
 
-label2.addEventListener ("click", function() {
-  if (radio2.checked === false) {
-    line.classList.toggle("example__switch-line--switched");
-  }
-});
+var slider = function () {
+  if (window.innerWidth < 768) {
+    label1.addEventListener ("click", function() {
+      if (radio1.checked === false) {
+        line.classList.toggle("example__switch-line--switched");
+      }
+    });
+
+    label2.addEventListener ("click", function() {
+      if (radio2.checked === false) {
+        line.classList.toggle("example__switch-line--switched");
+      }
+    });
 
 
-line.addEventListener ("click", function(evt) {
-  evt.preventDefault();
-  if (radio1.checked === true) {
-    radio2.checked = true;
-    line.classList.add("example__switch-line--switched");
-  }
+    line.addEventListener ("click", function(evt) {
+      evt.preventDefault();
+      if (radio1.checked === true) {
+        radio2.checked = true;
+        line.classList.add("example__switch-line--switched");
+      }
 
-  else if (radio2.checked === true) {
-    radio1.checked = true;
-    line.classList.remove("example__switch-line--switched");
-  }
+      else if (radio2.checked === true) {
+        radio1.checked = true;
+        line.classList.remove("example__switch-line--switched");
+      }
 
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].classList.toggle("example__slide--hidden");
-  }
-})
+      for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.toggle("example__slide--hidden");
+      }
+    })
 
-for (var i = 0; i < radios.length; i++) {
-  radios[i].onchange = function() {
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.toggle("example__slide--hidden");
+    for (var i = 0; i < radios.length; i++) {
+      radios[i].onchange = function() {
+        for (var i = 0; i < slides.length; i++) {
+          slides[i].classList.toggle("example__slide--hidden");
+        }
+      }
     }
   }
 }
+
+
+window.onresize = function() {
+  slider();
+}
+
+slider();
