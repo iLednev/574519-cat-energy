5a067792a629fe3a160f1431c83360b71509ec90
Lednev <iLednevii@gmail.com>
2018-11-12 23:26:46 +0300

    Исправлены замечания проверяющего

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,7 +1,25 @@
 var button = document.querySelector(".main-header__button");
 var nav = document.querySelector(".main-nav");
 
+var activate = function() {
+  if (window.innerWidth < 768) {
+    nav.classList.add("hide");
+    button.classList.add("show");
+  }
+
+  else if (window.innerWidth >= 768) {
+    nav.classList.remove("hide");
+    button.classList.remove("show");
+  }
+}
+
 button.onclick = function () {
   button.classList.toggle("active");
-  nav.classList.toggle("show");
+  nav.classList.toggle("hide");
+}
+
+activate();
+
+window.onresize = function () {
+  activate();
 }
